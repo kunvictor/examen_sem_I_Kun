ecdae6ddb7aa8b6743a2db8328ecd628749bebc2
kunvictor <victor.kun02@e-uvt.ro>
2025-01-12 14:14:00 +0200

    create table partial

---
+++ 1.create.sql
@@ -0,0 +1,15 @@
+create table if not exists tara
+(
+fid serial primary key,
+nume varchar(50) unique not null,
+suprafata decimal null
+);
+
+create table if not exists judete
+(
+fid serial primary key,
+nume varchar(50) unique not null,
+indicativ char(2) not null,
+suprafata decimal null
+tara_fid integer not null
+);
