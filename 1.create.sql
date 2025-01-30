2223876f9d5eff409102a7516eab3a8529dd5dd9
kunvictor <victor.kun02@e-uvt.ro>
2025-01-30 20:08:21 +0200

    ok

--- 1.create.sql
+++ 1.create.sql
@@ -1,16 +1,71 @@
-create table if not exists tara
-(
-fid serial primary key,
-nume varchar(50) unique not null,
-suprafata decimal null
+
+CREATE TABLE IF NOT EXISTS Pasaport (
+    Pasaport_id INT PRIMARY KEY,
+	numar_pasaport VARCHAR(50) UNIQUE NOT NULL,
+    tara VARCHAR(50) not null
 );
 
-create table if not exists judete
-(
-fid serial primary key,
-nume varchar(50) unique not null,
-indicativ char(2) not null,
-suprafata decimal null,
-tara_fid integer not null references tara(fid)
+CREATE TABLE IF NOT EXISTS Turist (
+    Turist_id INT PRIMARY KEY,
+    nume VARCHAR(100) NOT NULL,
+    email VARCHAR(100) NOT NULL,
+    telefon VARCHAR(10) NOT NULL,
+	Pasaport_id INT UNIQUE,
+    FOREIGN KEY (Pasaport_id) REFERENCES Pasaport(Pasaport_id)
+);
+
+CREATE TABLE IF NOT EXISTS Destinatii (
+    Destinatii_id INT PRIMARY KEY,
+    nume VARCHAR(100) not null,
+    tara VARCHAR(100) not null,
+    regiune VARCHAR(100) not null,
+    descriere TEXT
+);
+
+CREATE TABLE IF NOT EXISTS  Trasee_turistice (
+    trasee_turistice_id INT PRIMARY KEY,
+    nume VARCHAR(100) NOT NULL,
+    dificultate VARCHAR(50) NOT NULL,
+    lungine text,
+    Destinatii_id INT,
+    FOREIGN KEY (Destinatii_id) REFERENCES Destinatii(Destinatii_id)
+);
+
+
+CREATE TABLE IF NOT EXISTS Vizite (
+    Vizite_id INT PRIMARY KEY,
+    Turist_id INT,
+    trasee_turistice_id INT,
+    Sosire DATE,
+    FOREIGN KEY (Turist_id) REFERENCES Turist(Turist_ID),
+    FOREIGN KEY (trasee_turistice_id) REFERENCES trasee_turistice(trasee_turistice_id)
+);
+
+
+CREATE TABLE IF NOT EXISTS Resurse_naturale (
+    resurse_ID INT PRIMARY KEY,
+    nume VARCHAR(100) not null,
+    tip VARCHAR(50) not null,
+    descriere TEXT,
+    Destinatii_id INT,
+    FOREIGN KEY (Destinatii_id) REFERENCES Destinatii(Destinatii_id)
+);
+
+CREATE TABLE IF NOT EXISTS Hoteluri (
+    Hotel_id INT PRIMARY KEY,
+    nume VARCHAR(100) not null,
+    Stele VARCHAR(100) not null,
+    Destinatii_id INT,
+	descriere text,
+    FOREIGN KEY (Destinatii_id) REFERENCES Destinatii(Destinatii_id)
+);
+
+CREATE TABLE IF NOT EXISTS Evenimente (
+    Evenimente_id INT PRIMARY KEY,
+    nume VARCHAR(100) not null,
+    data DATE,
+    descriere TEXT,
+    Destinatii_id INT,
+    FOREIGN KEY (Destinatii_id) REFERENCES Destinatii(Destinatii_id)
 );
 
